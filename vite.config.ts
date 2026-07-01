8f3ad92a4feb1275ad2de3c144baa681505aa0cf
Pedro Henrique <71939458+Tdk17@users.noreply.github.com>
2026-07-01 20:18:35 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/ClinicFlow-CRM/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
